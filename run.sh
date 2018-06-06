6c9755e27bd4fb4d2b62f8805d3d1edb92aa7926
Zhang Huanjie <james@ustc.edu.cn>
2018-06-06 21:02:03 +0800

    fix_bug

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "国际一流高校对比<p>" > index.html
+echo "相关测试代码在<a href=https://github.com/bg6cq/checksite>https://github.com/bg6cq/checksite</a><p>" > index.html
+echo "说明：测试是否支持HTTP/2.0使用的是<a href=https://http2.pro/>https://http2.pro</a>，结果可能与实际不一致<p>" >> index.html
+echo "国际一流高校对比<p>" >> index.html
 echo "<table border=1 cellspacing=0>" >> index.html
 echo "<th>高校</th><th>网站</th><th>IPv6解析</th><th>IPv6访问</th><th>SSL</th><th>HTTP/2</th></tr>" >> index.html
 
