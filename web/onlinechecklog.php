39cf194313189d8d6a74a282a35943bd851d3cb2
Zhang Huanjie <james@ustc.edu.cn>
2023-04-18 10:39:24 +0800

    jquery update to 3.6.4

--- web/onlinechecklog.php
+++ web/onlinechecklog.php
@@ -66,7 +66,7 @@ $stmt->close();
 </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/jquery-3.6.4.slim.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src='js/jquery.dataTables.min.js'></script>
